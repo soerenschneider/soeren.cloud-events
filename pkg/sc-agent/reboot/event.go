af02b0e30c5839a5b7057738a5da2a71ffbde6e2
Sören Schneider <56670304+soerenschneider@users.noreply.github.com>
2025-04-21 11:49:15 +0200

    init

--- pkg/sc-agent/reboot/event.go
+++ pkg/sc-agent/reboot/event.go
@@ -1,7 +1,32 @@
 package secrets
 
+import (
+	"encoding/json"
+	"time"
+
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"github.com/google/uuid"
+	events "soeren.cloud-events/pkg"
+)
+
 type RebootData struct {
 	SecretName string `json:"secretName"`
 	Path       string `json:"path"`
 	Status     string `json:"status"`
+}
+
+func NewSystemRebootedEvent(source string, data RebootData) cloudevents.Event {
+	event := cloudevents.NewEvent()
+	event.SetSource(source)
+	event.SetType(string(events.TypeSystemRebooted))
+	event.SetID(uuid.New().String())
+	event.SetTime(time.Now())
+	event.SetSubject("os-reboot")
+	event.SetDataContentType(cloudevents.ApplicationJSON)
+	event.SetData(cloudevents.ApplicationJSON, data)
+	return event
+}
+
+func NewMarshalledSystemRebootEvent(source string, data RebootData) ([]byte, error) {
+	return json.Marshal(NewSystemRebootedEvent(source, data))
 }
